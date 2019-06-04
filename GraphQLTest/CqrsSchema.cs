e60c180fbe697ca77557d5c473297ae2c5490306
cime <cime3d@gmail.com>
2019-06-05 00:28:33 +0200

    Generated queries from CQRS

--- GraphQLTest/CqrsSchema.cs
+++ GraphQLTest/CqrsSchema.cs
@@ -40,9 +40,8 @@ namespace GraphQLTest
         private void RegisterCommands()
         {
             var registrations = _container.GetRootRegistrations();
-            var registeredTypes = registrations.Select(x => x.ServiceType).ToList();
+            var registeredTypes = registrations.Select(x => x.Registration.ImplementationType).Distinct().ToList();
             var commandHandlerTypes = registeredTypes.Where(x =>
-                !x.IsInterface &&
                 x.GetTypeInfo().IsAssignableToGenericType(typeof(ICommandHandler<,>))).ToList();
             
             
@@ -123,15 +122,82 @@ namespace GraphQLTest
         public void RegisterQueries()
         {
             var registrations = _container.GetRootRegistrations();
-            var registeredTypes = registrations.Select(x => x.ServiceType).ToList();
-            var queries = registeredTypes.Where(x =>
-                !x.IsInterface &&
+            var registeredTypes = registrations.Select(x => x.Registration.ImplementationType).Distinct().ToList();
+            var queryHandlerTypes = registeredTypes.Where(x =>
                 x.GetTypeInfo().IsAssignableToGenericType(typeof(IQueryHandler<,>))).ToList();
-
-
-            foreach (var query in queries)
+            
+            
+            foreach (var queryHandlerType in queryHandlerTypes)
             {
+                var descriptionAttribute = queryHandlerType.GetTypeInfo().GetCustomAttribute<DescriptionAttribute>();
+                var genericType = queryHandlerType.GetInterfaces().Single(x => x.GetTypeInfo().IsAssignableToGenericType(typeof(IQueryHandler<,>)));
+                var genericArguments = genericType.GetGenericArguments();
+                var queryType = genericArguments[0];
+                var resultType = genericArguments[1];
+
+                var exposeAttribute = queryType.GetCustomAttribute<ExposeAttribute>();
+
+                if (exposeAttribute == null)
+                {
+                    continue;
+                }
+
+                //
+                // For each command here I would like to create a mutation that has 1 input of type commandType and returns the resultType
+                // Example command can be found inside Commands/TestCommand.cs
+                //
+                // Each command can be resolved using: var result = (new "commandHandlerType"()).Handle("command instance");
+                //
+
+                var inputTypeName = queryType.Name;
+                var inputObjectType = typeof(InputObjectGraphType<>).MakeGenericType(queryType);
+                var inputGqlType = (IInputObjectGraphType)Activator.CreateInstance(inputObjectType);
+
+                inputGqlType.Name = inputTypeName;
+
+                var addFieldMethod = inputGqlType.GetType().GetMethod("AddField");
+                var properties = queryType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+                foreach (var propertyInfo in properties)
+                {
+                    addFieldMethod.Invoke(inputGqlType, new[]
+                    {
+                        new FieldType()
+                        {
+                            Name = CamelCase(propertyInfo.Name),
+                            Type = propertyInfo.PropertyType.GetGraphTypeFromType()
+                        }
+                    });
+                }
                 
+                var resultTypeName = resultType.Name;
+                var returnObjectType = typeof(AutoRegisteringObjectGraphType<>).MakeGenericType(resultType);
+                var resultGqlType = (IGraphType)Activator.CreateInstance(returnObjectType, null);
+                resultGqlType.Name = resultTypeName;
+                
+                var queryArgument = new QueryArgument(inputGqlType);
+                queryArgument.Name = "query";
+                
+                var queryQueryParameters = new List<QueryArgument>()
+                {
+                    queryArgument
+                };
+
+                var queryName = new Regex("Query").Replace(queryType.Name, "");
+
+                if (!Query.HasField(queryName))
+                {
+                    var type = new FieldType
+                    {
+                        ResolvedType = resultGqlType,
+                        Resolver = new QueryResolver(_container, queryHandlerType, queryType),
+                        Name = CamelCase(queryName),
+                        Description = descriptionAttribute?.Description,
+                        Arguments = new QueryArguments(queryQueryParameters)
+                    };
+
+                    Query.AddField(type);
+                }
             }
         }
         
